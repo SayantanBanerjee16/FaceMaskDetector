a6be9e3baaaa01a74a4ac52c3b575527d2062eba
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2020-06-25 21:33:29 +0530

    Face Detection code done

--- app/src/main/java/com/sayantanbanerjee/facedetection/MainActivity.kt
+++ app/src/main/java/com/sayantanbanerjee/facedetection/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.sayantanbanerjee.facedetection
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,7 +38,40 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_GALLERY_CODE) {
-            imageView.setImageURI(data?.data)
+            imageView.setImageURI(data!!.data)
+
+            // create Firebase Vision Image
+            val image: FirebaseVisionImage
+            try {
+                image = FirebaseVisionImage.fromFilePath(this, data.data!!)
+                Toast.makeText(this, getString(R.string.face_detecting), Toast.LENGTH_LONG).show()
+                // create Face Detector options
+                // ACCURATE OR HARD -> accuracy
+                // ALL_LANDMARKS OR NO_LANDMARKS -> identifying Eyes, cheeks, lips, mouth, ears etc
+                // CONTOUR OR NO_CONTOUR -> smiling & eyes open
+                val options =
+                    FirebaseVisionFaceDetectorOptions.Builder()
+                        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+                        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                        .build()
+
+                //Pass this options inside the Detector
+                val detector = FirebaseVision.getInstance()
+                    .getVisionFaceDetector(options)
+
+                //Pass the image to the final detector
+                val result =
+                    detector.detectInImage(image)
+                        .addOnSuccessListener {
+                            Toast.makeText(this, getString(R.string.face_found), Toast.LENGTH_LONG).show()
+                        }
+                        .addOnFailureListener {
+                            Toast.makeText(this, getString(R.string.face_not_found), Toast.LENGTH_LONG).show()
+                        }
+
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
         }
     }
 }
